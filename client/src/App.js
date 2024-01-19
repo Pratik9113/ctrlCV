c7cb6d2c061f831e57ab67843158663f9d864644
Pratik9113 <117252760+Pratik9113@users.noreply.github.com>
2024-01-19 22:47:07 +0530

    add tailwind css

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,10 @@
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
-import ScrollToTop from "react-scroll-to-top";
+import Home from './components/Home/Home';
+
 function App() {
- // const [theme] = useTheme();
   return (
     <>
-  
-      <Navbar/>
-    
-    <ScrollToTop smooth
-    color='#f29f67'
-    style={{backgroundColor:'#1e1e2c', borderRadius:'80px'}} />
+       <Home/>
   </>
   );
 }
